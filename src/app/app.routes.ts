4433b46aaae8d79d406c3df37fc6296b483a2108
Fabio Vinicius <fabiovinicius1@outlook.com>
2025-04-04 16:01:51 -0300

    :sparkles: feat: Página de contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'contato', component: ContatoComponent },
+];
