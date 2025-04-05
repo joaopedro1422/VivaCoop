4cc2cebd92744eacec324ca9b8838d7ee6532b75
Fabio Vinicius <fabiovinicius1@outlook.com>
2025-04-04 21:32:39 -0300

    :sparkles: feat: Página de  evento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContatoComponent } from './contato/contato.component';
+import { EventoComponent } from './evento/evento.component';
 
 export const routes: Routes = [
 	{ path: 'contato', component: ContatoComponent },
+	{ path: 'evento', component: EventoComponent },
 ];
