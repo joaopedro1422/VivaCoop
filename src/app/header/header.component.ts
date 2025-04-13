646f3adf67fe9582f86c1591cca9991f6a5ecc6e
Fabio Vinicius <fabiovinicius1@outlook.com>
2025-04-13 15:05:56 -0300

    fix: correção no gerenciamento de rotas

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+	selector: 'app-header',
+	standalone: true,
+	imports: [],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+	constructor(private router: Router) { }
 
+	navegarParaHome() {
+		this.router.navigate(['/home']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaSobre() {
+		this.router.navigate(['/sobre']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaServicos() {
+		this.router.navigate(['/servicos']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaColaboradores() {
+		this.router.navigate(['/colaboradores']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaParcerias() {
+		this.router.navigate(['/parcerias']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaNoticias() {
+		this.router.navigate(['/noticia']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaEventos() {
+		this.router.navigate(['/evento']).then(() => window.scrollTo(0, 0));
+	}
+
+	navegarParaContato() {
+		this.router.navigate(['/contato']).then(() => window.scrollTo(0, 0));
+	}
 }
