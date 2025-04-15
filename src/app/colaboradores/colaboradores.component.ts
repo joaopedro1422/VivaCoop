ebe0789a18a69c415d6713a58887c43f79c55084
fabiovinicius1 <52680813+fabiovinicius1@users.noreply.github.com>
2025-04-15 05:34:15 -0300

    Revert "Login page"

--- src/app/colaboradores/colaboradores.component.ts
+++ src/app/colaboradores/colaboradores.component.ts
@@ -2,8 +2,8 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { ColaboradoresService, Colaborador } from '../colaboradores-servico/colaboradores.service';
 import { CommonModule } from '@angular/common';
-import  { AutenticacaoService } from '../services/autenticacao.service';
-import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-colaboradores',
   imports: [CommonModule],
@@ -11,7 +11,6 @@ import { Router } from '@angular/router';
   styleUrl: './colaboradores.component.css'
 })
 export class ColaboradoresComponent {
-  userName: string | null = null;
   @ViewChild('formulario') formulario!: ElementRef;
 
   rolarParaFormulario(): void {
@@ -20,23 +19,11 @@ export class ColaboradoresComponent {
 
   colaboradores: Colaborador[] = [];
 
-  constructor(private colaboradoresService: ColaboradoresService, private authService: AutenticacaoService, private router: Router) {}
+  constructor(private colaboradoresService: ColaboradoresService) {}
 
   ngOnInit() {
     this.colaboradoresService.getColaboradores().subscribe(data => {
       this.colaboradores = data;
     });
-    this.authService.loggedInUser$.subscribe((user) => {
-      this.userName = user?.name || null;  // Atualiza o nome do usuário no header
-    });
-  }
-
-  verificaLogin(){
-    if(this.userName){
-      this.router.navigate(['/area-colaborador']);
-    }
-    else{
-      this.router.navigate(['/login']);
-    }
   }
 }
